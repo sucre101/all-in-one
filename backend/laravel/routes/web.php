47367b452001c98907121dbe10843c809f60be31
Alex <vaniulen84@gmail.com>
2023-11-06 00:17:34 +0300

    Upload default migration from laravel app. Create UserController and define base url route and link to created route. Change php image and down downgrade version, because some php extensions not include upper version fpm image. Need another solution... Add redis image, but not configurate him.

--- backend/laravel/routes/web.php
+++ backend/laravel/routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return \response()->json(['success' => true], 200);
-});
+Route::get('/', ['App\Http\Controllers\UserController', 'index']);
